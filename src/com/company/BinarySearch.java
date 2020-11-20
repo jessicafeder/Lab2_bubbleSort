967209e2dae41c90d0810341dcb64b0991fcf52f
Anders Wenström <anders.wenstrom@student.kyh.se>
2020-11-20 15:56:02 +0100

    fortsättning på remove och att hitta högsta värde

--- src/com/company/BinarySearch.java
+++ src/com/company/BinarySearch.java
@@ -3,6 +3,26 @@ package com.company;
 public class BinarySearch {
     Node root;
 
+    class Node {
+        int key;
+        String name;
+
+        Node leftChild;
+        Node rightChild;
+
+
+        Node(int key, String name) {
+
+            this.key = key;
+            this.name = name;
+        }
+
+        public String toString() {
+            return name + " has a key " + key;
+        }
+
+    }
+
     public void addNode(int key, String name) {
 
 
@@ -33,12 +53,71 @@ public class BinarySearch {
 
         }
     }
+    /*
+    Vilka fall för remove finns?
+    1. Elementet finns i rootnoden och rootnoden har inga barn
+    2. Elmentet finns i en lövnod
+    3. Elementet finns en nod med ett barn
+    3. Elementet finns i en nod med två barn
+     */
 
-    public void remove(Node root, int x) {
+    private int findMaxValue(Node tree){
+        /*
+              tree
+    mindre->l      r<-större
+         */
+        if(tree.rightChild == null){
+            return tree.key;
+        }
+        else{
+            return findMaxValue(tree.rightChild);
+        }
+    }
+
+    private int MaxValue(Node root) {
+        int MaxValue = root.key;
+        while (root.rightChild != null) {
+            MaxValue = root.rightChild.key;
+            root = root.leftChild;
+        }
+        return MaxValue;
+
+    }
+
+    public void remove(int key){
+        root = remove_rec(root, key);
+    }
+
+
+
+
+    private Node remove_rec(Node tree, int key){
+        /*
+
+         */
+        if (tree == null)
+            return tree;
+        if (key < tree.key)
+            tree.leftChild = remove_rec(tree.leftChild, key);
+        else if (key > tree.key)
+            tree.rightChild = remove_rec(tree.rightChild, key);
+        else {
+            if (tree.leftChild == null)
+                return tree.rightChild;
+            else if (tree.rightChild == null)
+                return tree.leftChild;
+
+            tree.key = findMaxValue(tree.leftChild);
+            tree.name = findNode(tree.key).name;
+            tree.leftChild = remove_rec(tree.leftChild, tree.key);
+        }
+        return tree;
+    }
+    public void remove2( int x) {
         if (x == this.root.key && this.root.leftChild == null && this.root.rightChild == null) {
             this.root = null;
         } else {
-            remove(this.root, x);
+            remove( x);
         }
     }
 
@@ -108,25 +187,7 @@ public class BinarySearch {
         }
     }
 
-    class Node {
-        int key;
-        String name;
 
-        Node leftChild;
-        Node rightChild;
-
-
-        Node(int key, String name) {
-
-            this.key = key;
-            this.name = name;
-        }
-
-        public String toString() {
-            return name + " has a key " + key;
-        }
-
-    }
 
     public Node findNode(int key) {
         Node focusNode = root;
@@ -152,17 +213,21 @@ public class BinarySearch {
         theTree.addNode(55,"Child 3");
         theTree.addNode(58,"Child 4");
         theTree.addNode(30,"Child 5");
-        theTree.addNode(25,"Child 6");
+        theTree.addNode(70,"Child 6");
         theTree.addNode(110,"Child 7");
         theTree.addNode(108,"Child 8");
         theTree.addNode(111,"Child 9");
         System.out.println(theTree.findNode(30)); // döpt get method till findNode
-
+        System.out.println("------------------------------------------");
         //theTree.PreorderTraverseTree(theTree.root);
-        //theTree.inOrderTraverseTree(theTree.root);
-        theTree.PostorderTraverseTree(theTree.root);
-        System.out.println("Search for 30 ");
-        System.out.println(theTree.findNode(30));
+        theTree.inOrderTraverseTree(theTree.root);
+        //theTree.PostorderTraverseTree(theTree.root);
+        //System.out.println("Search for 30 ");
+        //System.out.println(theTree.findNode(30));
+        System.out.println("--------------------------------------------");
+        theTree.remove(65);
+        theTree.inOrderTraverseTree(theTree.root);
+
 
 
                     /*100
